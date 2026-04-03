1cc62d7669df60485ab312c149ecc7362571ae03
Antonio72Dev <toinhoamaralaa@gmail.com>
2026-04-02 19:20:17 -0700

    Resolve merge conflicts

--- flatfinder/src/app/app.config.ts
+++ flatfinder/src/app/app.config.ts
@@ -1,20 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
-};
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
     provideRouter(appRoutes)
   ]
 };
