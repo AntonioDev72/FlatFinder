a2628b8407b48b802975473a852d2118ce18f299
Antonio72Dev <toinhoamaralaa@gmail.com>
2026-04-02 19:39:34 -0700

    Resolve angular conflits

--- flatfinder/src/app/app.ts
+++ flatfinder/src/app/app.ts
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-=======
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
->>>>>>> origin/dev-b
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
+
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
@@ -17,10 +12,3 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 export class App {
   protected readonly title = signal('flatfinder');
 }
-=======
-  imports: [RouterOutlet, RouterLink],
-  templateUrl: './app.html',
-  styleUrls: ['./app.scss']
-})
-export class AppComponent {}
->>>>>>> origin/dev-b
